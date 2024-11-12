cfa450304ebee17a3d7e1b6d6eafbc42fc130e0d
BA Pranava <97015345+pranava-ba@users.noreply.github.com>
2024-11-12 23:03:36 +0530

    day two

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
+//day one
+
+/*
 #include <iostream>
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+*/
+//day two
+#include <iostream>  // Required for input/output
+
+int main() {
+    int num;
+    std::cout << "Enter a number: ";
+    std::cin >> num;  // User inputs a number
+
+    std::cout << "You entered: " << num << std::endl;
     return 0;
 }
